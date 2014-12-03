8a2f919117268d419055fed4fef9ade2409f86c2
Tim Hurley <tim.hurley@finance.gov.au>
2014-12-04 10:46:11 +1100

    homepage local gov now list not grid

--- v2/homepage.php
+++ v2/homepage.php
@@ -14,7 +14,6 @@
             <div class="media-body">
               <h3 class="media-heading"><a href="http://australia.gov.au/">australia.gov.au</a></h3>
               <p>Entry point for people to find Australian Government information. Also includes links to popular state, territory and local government information.</p>
-              <!-- <a href="http://australia.gov.au/" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
             </div>
           </div>
         </div>
@@ -31,7 +30,6 @@
             <div class="media-body">
               <h3 class="media-heading"><a href="http://business.gov.au/">business.gov.au</a></h3>
               <p>Helps businesses to find all hte information and tools they need from government to start, run and grow a business.</p>
-              <!-- <a href="http://business.gov.au/" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
             </div>
           </div>
         </div>
@@ -48,7 +46,6 @@
             <div class="media-body">
               <h3 class="media-heading"><a href="http://my.gov.au/">my.gov.au</a></h3>
               <p>A secure myGov account lets you link a range of Australian Government services with one username and password, all in one place.</p>
-              <!-- <a href="http://my.gov.au/" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
             </div>
           </div>
         </div>
@@ -80,7 +77,6 @@
             </span>
             <div class="media-body">
               <h4 class="media-heading"><a href="http://www.act.gov.au/">www.act.gov.au</a></h4>
-              <!-- <a href="http://www.act.gov.au/" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
             </div>
           </div>
         </div>
@@ -99,7 +95,6 @@
             </span>
             <div class="media-body">
               <h4 class="media-heading"><a href="http://nsw.gov.au/">nsw.gov.au</a></h4>
-              <!-- <a href="http://nsw.gov.au/" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
             </div>
           </div>
         </div>
@@ -118,7 +113,6 @@
             </span>
             <div class="media-body">
               <h4 class="media-heading"><a href="http://nt.gov.au/">nt.gov.au</a></h4>
-              <!-- <a href="http://nt.gov.au/" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
             </div>
           </div>
         </div>
@@ -137,7 +131,6 @@
             </span>
             <div class="media-body">
               <h4 class="media-heading"><a href="http://qld.gov.au/">qld.gov.au</a></h4>
-              <!-- <a href="http://qld.gov.au/" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
             </div>
           </div>
         </div>
@@ -156,7 +149,6 @@
             </span>
             <div class="media-body">
               <h4 class="media-heading"><a href="http://sa.gov.au/">sa.gov.au</a></h4>
-              <!-- <a href="http://sa.gov.au/" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
             </div>
           </div>
         </div>
@@ -175,7 +167,6 @@
             </span>
             <div class="media-body">
               <h4 class="media-heading"><a href="http://tas.gov.au/">tas.gov.au</a></h4>
-              <!-- <a href="http://tas.gov.au/" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
             </div>
           </div>
         </div>
@@ -194,7 +185,6 @@
             </span>
             <div class="media-body">
               <h4 class="media-heading"><a href="http://vic.gov.au/">vic.gov.au</a></h4>
-              <!-- <a href="http://vic.gov.au/" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
             </div>
           </div>
         </div>
@@ -213,7 +203,6 @@
             </span>
             <div class="media-body">
               <h4 class="media-heading"><a href="http://wa.gov.au/">wa.gov.au</a></h4>
-              <!-- <a href="http://wa.gov.au/" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
             </div>
           </div>
         </div>
@@ -236,92 +225,13 @@
       The <a href="http://www.alga.asn.au/">Australian Local Government Association</a> is the national voice of local government,
       representing 560 councils across the country. State local government directories provide links to local council sites:
     </p>
-  </div>
-  <!-- <h3>Local government directories</h3> -->
-  <div class="row">
-
-    <div class="col-sm-6 col-md-4">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title">New South Wales</h3>
-        </div>
-        <div class="panel-body">
-          <a class="name" href="http://www.olg.nsw.gov.au/"><h4>Office of Local Government</h4></a>
-          <!-- <a href="http://www.olg.nsw.gov.au/" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
-        </div>
-      </div>
-    </div>
-
-    <div class="col-sm-6 col-md-4">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title">Northern Territory</h3>
-        </div>
-        <div class="panel-body">
-          <a class="name" href="http://www.localgovernment.nt.gov.au/home/council_information"><h4>Department of Local Government and Regions</h4></a>
-          <!-- <a href="http://www.localgovernment.nt.gov.au/home/council_information" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
-        </div>
-      </div>
-    </div>
-
-    <div class="col-sm-6 col-md-4">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title">Queensland</h3>
-        </div>
-        <div class="panel-body">
-          <a class="name" href="http://www.dlg.qld.gov.au/local-government-directory.html"><h4>Department of Local Government, Community Recovery and Resilience</h4></a>
-          <!-- <a href="http://www.dlg.qld.gov.au/local-government-directory.html" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
-        </div>
-      </div>
-    </div>
-
-    <div class="col-sm-6 col-md-4">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title">South Australia</h3>
-        </div>
-        <div class="panel-body">
-          <a class="name" href="http://www.lga.sa.gov.au/page.aspx?u=210"><h4>Local Government Association of South Australia</h4></a>
-          <!-- <a href="http://www.lga.sa.gov.au/page.aspx?u=210" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
-        </div>
-      </div>
-    </div>
-
-    <div class="col-sm-6 col-md-4">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title">Tasmania</h3>
-        </div>
-        <div class="panel-body">
-          <a class="name" href="http://www.dpac.tas.gov.au/divisions/local_government/local_government_directory"><h4>Department of Premier and Cabinet</h4></a>
-          <!-- <a href="http://www.dpac.tas.gov.au/divisions/local_government/local_government_directory" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
-        </div>
-      </div>
-    </div>
-
-    <div class="col-sm-6 col-md-4">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title">Victoria</h3>
-        </div>
-        <div class="panel-body">
-          <a class="name" href="http://www.dtpli.vic.gov.au/local-government"><h4>Department of Transport, Planning and Local Infrastructure</h4></a>
-          <!-- <a href="http://www.dtpli.vic.gov.au/local-government" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
-        </div>
-      </div>
-    </div>
-
-    <div class="col-sm-6 col-md-4">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title">Western Australia</h3>
-        </div>
-        <div class="panel-body">
-          <a class="name" href="http://www.dlg.wa.gov.au/Content/Directory/Default.aspx"><h4>Department of Local Government and Communities</h4></a>
-          <!-- <a href="http://www.dlg.wa.gov.au/Content/Directory/Default.aspx" class="btn btn-primary pull-right btn-outline" role="button">Visit site &raquo;</a> -->
-        </div>
-      </div>
-    </div>
-
+    <ul class="lead">
+      <li><a href="http://www.olg.nsw.gov.au/">New South Wales local government directory</a></li>
+      <li><a href="http://www.localgovernment.nt.gov.au/home/council_information">Northern Territory local council information</a></li>
+      <li><a href="http://www.dlg.qld.gov.au/local-government-directory.html">Queensland local government directory</a></li>
+      <li><a href="http://www.lga.sa.gov.au/page.aspx?u=210">South Australia local council information</a></li>
+      <li><a href="http://www.dpac.tas.gov.au/divisions/local_government/local_government_directory">Tasmania local government directory</a></li>
+      <li><a href="http://www.dtpli.vic.gov.au/local-government">Victoria local council information</a></li>
+      <li><a href="http://www.dlg.wa.gov.au/Content/Directory/">Western Australia local government directory</a></li>
+    </ul>
   </div>
