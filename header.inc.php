8e4d8be6da0c95e92758ce83fe6cd2795f07a769
Tim Hurley <tim.hurley@finance.gov.au>
2014-12-05 12:24:17 +1100

    IE8BS

--- header.inc.php
+++ header.inc.php
@@ -32,6 +32,7 @@ $base_url = "./";
   <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <link href="ie8.css" rel="stylesheet">
   <![endif]-->
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
